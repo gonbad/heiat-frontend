73adaafcf835a5dabea98ed47489a3cb50dae803
Omid Baharloo <omid.gonbad@gmail.com>
2018-09-12 17:00:34 +0430

    profile page created

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
@@ -13,10 +14,16 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
-        }, {
+        },
+        {
             path: '/login',
             name: 'Login',
             component: Login
+        },
+        {
+            path: '/profile',
+            name: 'Profile',
+            component: Profile
         }
         // {
         //   path: '/about',
---
+++ src/views/Profile.vue
@@ -0,0 +1,35 @@
+/**
+* Created by vouill on 11/13/17.
+*/
+
+<template>
+    <div>
+       dd
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+    import {AUTH_REQUEST} from '@/modules/constants'
+
+    export default {
+        name: 'Profile',
+        data () {
+            return {
+                username: '4900152234',
+                password: '2rooohet',
+            }
+        },
+        methods: {
+            login: function () {
+                const { username, password } = this
+                this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
+                    this.$router.push('/')
+                })
+            }
+        },
+    }
+</script>
