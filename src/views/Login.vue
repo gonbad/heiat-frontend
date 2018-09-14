e9e3f2ec567b8bbee1ed8b69be9cd0e72b9bcb4c
Omid Baharloo <omid.gonbad@gmail.com>
2018-09-14 19:05:58 +0430

    login enhanced

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,24 +4,43 @@
 
 <template>
     <div>
-        <form class="login" @submit.prevent="login">
-            <h1>Sign in</h1>
-            <label>User name</label>
-            <input required v-model="username" type="text" placeholder="Snoopy"/>
-            <label>Password</label>
-            <input required v-model="password" type="password" placeholder="Password"/>
-            <hr/>
-            <button type="submit">Login</button>
-        </form>
-        <p>
-            حساب کاربری ندارید؟ هم‌اکنون
-            <router-link :to="{name:'SignUp'}">ایجاد کنید</router-link>
-        </p>
-        <p>
-            <router-link :to="{name:'ResetPassword'}">
-                گذرواژه خود را فراموش کرده‌اید؟
-            </router-link>
-        </p>
+        <b-container>
+            <b-form @submit="onSubmit" v-if="status!=='success'">
+                <b-form-group label="کدملی:">
+                    <b-form-input type="text"
+                                  v-model="username"
+                                  placeholder="کدملی خود را وارد کنید">
+                    </b-form-input>
+                    <span class="error" v-for="e in error.username">{{e}}</span>
+                </b-form-group>
+                <b-form-group label="گذرواژه:"
+                              description="گذرواژه باید حداقل ۸ حرف باشد و ترکیبی از اعداد و حروف باشد">
+                    <b-form-input v-model="password" type="password"
+                                  placeholder="گذرواژه را وارد کنید">
+                    </b-form-input>
+                    <span class="error" v-for="e in error.password">{{e}}</span>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary" :disabled="status==='sending'">
+                    <span v-show="status!=='sending'">ورود</span>
+                    <span v-show="status==='sending'">در حال ارسال</span>
+                </b-button>
+                <p class="error" v-if="status==='error'">
+                    ورود با مشکل مواجه شد
+                </p>
+            </b-form>
+            <p>
+                حساب کاربری ندارید؟ هم‌اکنون
+                <router-link :to="{name:'SignUp'}">ایجاد کنید</router-link>
+            </p>
+            <p>
+                <router-link :to="{name:'ResetPassword'}">
+                    گذرواژه خود را فراموش کرده‌اید؟
+                </router-link>
+            </p>
+
+        </b-container>
+
     </div>
 </template>
 
@@ -41,17 +60,26 @@
         name: 'Login',
         data () {
             return {
-                username: '4900152234',
-                password: '2rooohet',
+                status:'default',
+                username: '',
+                password: '',
+                error:{
+                    username: [],
+                    password: [],
+                }
             }
         },
         methods: {
-            login: function () {
+            onSubmit: function (e) {
+                e.preventDefault()
+                this.status='sending'
                 const { username, password } = this
                 this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
-                    console.log('resolved');
+                    this.status='success'
                     this.$router.push('/')
-                    console.log('after push');
+                }).catch(error=>{
+                    this.error=error.response.data
+                    this.status='error'
                 })
             }
         },
